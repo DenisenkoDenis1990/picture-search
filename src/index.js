c324e5abb53585a9694e0d19a8ae40e943e10a82
DenisenkoDenis1990 <denisenko1990@gmail.com>
2022-08-22 14:33:32 +0300

    api logic creating

--- src/index.js
+++ src/index.js
@@ -1,44 +1,57 @@
 const axios = require('axios');
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import bootstrap from 'bootstrap';
+import ImageServiceApi from './images-service';
+import Notiflix from 'notiflix';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-more'),
 };
 
+const imageServiceApi = new ImageServiceApi();
+
 refs.searchForm.addEventListener('submit', OnSearch);
+refs.loadMoreButton.addEventListener('click', onClick);
+
+function onClick() {
+  imageServiceApi.pageIncrement();
+  imageServiceApi.getImages().then(response => {
+    if (imageServiceApi.totalHits >= response.data.totalHits) {
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.loadMoreButton.setAttribute('hidden', 'hidden');
+    }
+    addGalleryMarkUp(response.data.hits);
+  });
+}
 
 function OnSearch(e) {
   e.preventDefault();
-  const searchQuery = e.currentTarget.elements.searchQuery.value;
-  getImages(searchQuery);
-}
-
-async function getImages(query) {
-  try {
-    const params = {
-      key: '29310891-e344a11b8695986423724ef53',
-      q: query,
-      per_page: 40,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    };
-    const response = await axios.get('https://pixabay.com/api/', { params });
+  imageServiceApi.query = e.currentTarget.elements.searchQuery.value;
+  imageServiceApi.resetPage();
+  resetMarkup();
+  imageServiceApi.getImages().then(response => {
+    if (response.data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
     addGalleryMarkUp(response.data.hits);
-
-    console.log(galleryMarkUp);
-  } catch (error) {
-    console.log(error);
-  }
+    Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+    refs.loadMoreButton.removeAttribute('hidden');
+  });
 }
 
 function addGalleryMarkUp(pictures) {
   let galleryMarkUp = pictures
     .map(picture => {
       return `<div class="photo-card">
-  <a class="gallery-item" href ="${picture.largeImageURL}"><img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" width = 300px height = 300px/></a>
+  <a class="gallery-item" href ="${picture.largeImageURL}"><img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -61,6 +74,21 @@ function addGalleryMarkUp(pictures) {
     })
     .join('');
 
-  refs.gallery.innerHTML = galleryMarkUp;
+  refs.gallery.insertAdjacentHTML('beforeend', galleryMarkUp);
   let gallery = new SimpleLightbox('.gallery a');
+  if (imageServiceApi.page != 1) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    gallery.refresh();
+  }
+}
+
+function resetMarkup() {
+  refs.gallery.innerHTML = '';
 }
